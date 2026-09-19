fe69d224c49420de288cf6d160968ca7c2d34337
K Santhosh Kumar <codewithsanthosh.official@gmail.com>
2026-09-19 20:23:01 +0530

    Folder Structure - arranged

---
+++ NetworkDemo/NetworkDemo/Resources/Guide.swift
@@ -0,0 +1,53 @@
+//
+//  ContentView.swift
+//  NetworkDemo
+//
+//  Created by Koneti Santhosh Kumar on 9/19/26.
+//
+
+
+/*
+ 
+ SwiftUI MVVM project Folder Structure
+ 
+ 
+ MyApp/
+ │
+ ├── App/
+ │   └── MyApp.swift
+ │
+ ├── Core/
+ │   ├── Config/
+ │   │   └── AppConfig.swift
+ │   │
+ │   ├── Network/
+ │   │   └── APIClient.swift
+ │   │
+ │   ├── Constants/
+ │   │   └── AppConstants.swift
+ │   │
+ │   ├── Extensions/
+ │   │   └── View+Extensions.swift
+ │   │
+ │   └── Utils/
+ │       └── DateUtils.swift
+ │
+ ├── Common/
+ │   └── Components/
+ │       └── LoadingView.swift
+ │
+ ├── Features/
+ │   └── Posts/
+ │       ├── Models/
+ │       │   └── Post.swift
+ │       │
+ │       ├── Views/
+ │       │   └── PostsView.swift
+ │       │
+ │       └── ViewModels/
+ │           └── PostsViewModel.swift
+ │
+ └── Resources/
+     └── Assets.xcassets
+ 
+ */
